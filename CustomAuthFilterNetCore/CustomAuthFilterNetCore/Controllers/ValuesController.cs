a2364cf5297215f5926d9f6e82beb2336c8ceb37
HARSH <HARSH@DESKTOP-OHRP5NN>
2023-10-05 19:24:16 +0530

    initital checkin

---
+++ CustomAuthFilterNetCore/CustomAuthFilterNetCore/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomAuthFilterNetCore.Controllers
+{
+    [CustomAuthorizationFilter]
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class ValuesController : ControllerBase
+    {
+
+        public IActionResult GetData()
+        {
+            return Ok("Dev Tech Mind");
+        }
+    }
+}
---
+++ CustomAuthFilterNetCore/CustomAuthFilterNetCore/CustomAuthorizationFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CustomAuthFilterNetCore
+{
+    public class CustomAuthorizationFilter : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if(context != null)
+            {
+                Microsoft.Extensions.Primitives.StringValues authToken;
+                context.HttpContext.Request.Headers.TryGetValue("Authorization", out authToken);
+
+                var token = authToken.FirstOrDefault();
+
+                if(token == null)
+                {
+                    context.Result = new UnauthorizedObjectResult(string.Empty);
+                    return;
+                }
+            }
+        }
+    }
+}
